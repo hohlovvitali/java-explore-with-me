a7f97d2ad639cf50ec1bfde7986032cccbb6c7d8
hohlovvitali <b1aderaven@yandex.by>
2025-02-16 23:44:59 +0300

    checkstyle

--- ewm-main/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
+++ ewm-main/src/main/java/ru/practicum/ewm/event/service/EventServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.event.service;
 
 import com.querydsl.core.BooleanBuilder;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
